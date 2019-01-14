24d65acf5cf296cdff9c846bd82af21c224f058f
user <Wakatsuki@MacBook-Air.local>
2019-01-14 22:22:16 +0900

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-danger">
+                <div class="panel-heading">Welcome</div>
+
+                <div class="panel-body">
+                    ようこそ素晴らしきLaravelの世界へ！！
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
